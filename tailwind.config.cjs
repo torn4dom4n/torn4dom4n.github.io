c62c55b8c0ff004a16c765052b99158023e45e85
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-01-04 16:53:44 +0700

    1.1.0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,4 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Iosevka", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
 };

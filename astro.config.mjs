350877d589751585ad6db2cf04669c003e7e715a
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-01-05 00:29:18 +0700

    Initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     sitemap(),
     tailwind(),
     image({
-      serviceEntryPoint: "@astrojs/image/sharp",
+      serviceEntryPoint: "@astrojs/image/sharp"
     }),
   ],
 });

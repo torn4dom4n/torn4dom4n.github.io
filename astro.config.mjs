accb0643395c3c3024bfae9d78df66d59855a41c
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-01-04 23:59:47 +0700

    v1.2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
 export default defineConfig({
   site: "https://torn4dom4n.github.io",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });

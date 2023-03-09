09b0e759749c4b784eac53784405719a1e8798d5
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-03-10 00:14:53 +0700

    bump Astro2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 export default defineConfig({
   site: "https://torn4dom4n.github.io",
-  integrations: [
-    sitemap(),
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  experimental: {
+    assets: true,
+  },
+  integrations: [sitemap(), tailwind()],
 });

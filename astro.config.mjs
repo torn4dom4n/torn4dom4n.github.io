b9c0ac089633d7c3e0bbed11c63f3e6a2ad4d843
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-04-14 01:42:22 +0700

    v23.04.14

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://torn4dom4n.github.io',
   experimental: {
     assets: true,
   },

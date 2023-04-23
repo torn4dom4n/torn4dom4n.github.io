99442a522cef24e177114bcf41235bb3a18e9008
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-04-24 02:46:41 +0700

    support framer-motion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://torn4dom4n.github.io',
   experimental: {
     assets: true,
   },
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), react()],
 });

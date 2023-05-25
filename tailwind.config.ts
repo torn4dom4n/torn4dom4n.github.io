5131eeff1d15ef9de5711b8fa6846891094c8158
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-05-26 01:24:37 +0700

    chore: use typescript if it's possible

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
 
-module.exports = {
+export default {
   darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {

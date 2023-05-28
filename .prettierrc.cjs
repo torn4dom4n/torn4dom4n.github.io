fa24b054381dea47d2c0de330291795ee52d22f5
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-05-29 03:17:43 +0700

    v23.5.29

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,5 +3,18 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  importOrder: [
+    '',
+    '<BUILTIN_MODULES>',
+    '',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^[./]',
+  ],
+  importOrderTypeScriptVersion: '5.0.0',
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
 };
--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://torn4dom4n.github.io',
   experimental: {
     assets: true,
   },
-  integrations: [sitemap(), tailwind(), react()],
+  integrations: [react(), sitemap(), tailwind()],
 });

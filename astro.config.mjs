b5893cb9848e0d46261496a478cc9a237768e58c
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-06-23 23:36:06 +0700

    chore: update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://torn4dom4n.github.io',
   experimental: {

b035e5d701afd50920be35aa4ad0f3c2ef85bb9f
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-31 23:48:09 +0700

    chore: prettier v3

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+};

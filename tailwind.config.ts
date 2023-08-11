35c23c59de477be57eb7880b6344d0241f986feb
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-08-11 23:38:29 +0700

    v23.8.12 :sun:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
-/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],

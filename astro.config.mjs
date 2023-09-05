2830209bcae6805bd81210e73b0707a788f83810
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-05 15:23:33 +0700

    chore: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({

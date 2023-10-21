ef9c864a165872cb0e825dd1cfd747db9d5bccab
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-10-21 22:59:54 +0700

    feat: get site's config

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  name: "Long Nhat Nguyen | Website",
+  url: "https://torn4dom4n.github.io",
+  ogImage: "https://torn4dom4n.github.io/og-image.jpg",
+  description:
+    "Welcome to Long Nhat Nguyen's website",
+  links: {
+    email: "mailto:torn4dom4n@gmail.com",
+    github: "https://github.com/torn4dom4n",
+    youtube: "https://www.youtube.com/@torn4dom4n",
+    facebook: "https://facebook.com/torn4dom4n",
+    instagram: "https://instagram.com/torn4dom4n",
+    X: "https://twitter.com/torn4dom4n",
+    linkedin: "https://linkedin.com/in/torn4dom4n",
+    reddit: "https://reddit.com/user/torn4dom4n",
+    pinterest: "https://pinterest.com/torn4dom4n",
+    soundcloud: "https://soundcloud.com/torn4dom4n",
+    mastodon: "https://mstdn.social/@torn4dom4n",
+  },
+}
+
+export type SiteConfig = typeof siteConfig

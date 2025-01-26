885ea28834ac45bbca90ee79eb8e0a4f2db836c8
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-01-26 22:30:44 +0700

    chore: update Tailwind CSS integration and styles, remove deprecated config

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import { FontaineTransform } from 'fontaine';
 
 const VERCEL_PREVIEW_SITE =
@@ -14,9 +15,10 @@ const site = VERCEL_PREVIEW_SITE || 'https://torn4dom4n.github.io';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? site : 'http://localhost:4321',
-  integrations: [react(), sitemap(), tailwind({ applyBaseStyles: false })],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [
+      tailwindcss(),
       FontaineTransform.vite({
         fallbacks: ['Arial'],
         resolvePath: (id) => new URL(`./public${id}`, import.meta.url),

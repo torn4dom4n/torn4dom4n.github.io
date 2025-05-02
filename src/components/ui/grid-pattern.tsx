1f786e3942f4d4d819b17b2faa347566b4e86fb2
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-05-02 16:23:58 +0700

    v25.05.12 (#204)

---
+++ src/components/ui/grid-pattern.tsx
@@ -0,0 +1,55 @@
+import { cn } from "@/lib/utils";
+import { useId } from "react";
+
+interface GridPatternProps extends React.SVGProps<SVGSVGElement> {
+  width?: number;
+  height?: number;
+  x?: number;
+  y?: number;
+  strokeDasharray?: string;
+  patternTransform?: string;
+  className?: string;
+}
+
+export function GridPattern({
+  width = 40,
+  height = 40,
+  x = 0,
+  y = 0,
+  strokeDasharray = "0",
+  patternTransform,
+  className,
+  ...props
+}: GridPatternProps) {
+  const id = useId();
+
+  return (
+    <svg
+      aria-hidden="true"
+      className={cn(
+        "absolute inset-0 h-full w-full fill-none stroke-gray-300/20 dark:stroke-white/5",
+        className,
+      )}
+      {...props}
+    >
+      <defs>
+        <pattern
+          id={id}
+          width={width}
+          height={height}
+          patternUnits="userSpaceOnUse"
+          x={x}
+          y={y}
+          patternTransform={patternTransform}
+        >
+          <path
+            d={`M0 ${height} V0 H${width}`}
+            fill="none"
+            strokeDasharray={strokeDasharray}
+          />
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" fill={`url(#${id})`} />
+    </svg>
+  );
+}

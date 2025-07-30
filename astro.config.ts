b56fe3de6d61776c384d309911b0cb8de1ebe4f2
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 11:03:45 +0700

    Setup Vercel

--- astro.config.ts
+++ astro.config.ts
@@ -6,12 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
-const site =
-  process.env.CI &&
-  process.env.VERCEL_ENV !== "production" &&
-  process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : (process.env.SITE ?? "http://localhost:4321");
+const site = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : process.env.SITE ?? "http://localhost:4321";
 const base = "/";
 
 // https://astro.build/config

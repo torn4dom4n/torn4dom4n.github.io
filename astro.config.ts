093d49aecfd4f184ccd58ec9866daedf2c401294
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 10:32:11 +0700

    fix ci

--- astro.config.ts
+++ astro.config.ts
@@ -6,16 +6,18 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
-const site = process.env.CI
-  ? process.env.VERCEL_ENV !== "production" && process.env.VERCEL_URL
+const site =
+  process.env.CI &&
+  process.env.VERCEL_ENV !== "production" &&
+  process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
-    : const site = process.env.SITE;
-  : "http://localhost:4321";
-const base = process.env.BASE;
+    : process.env.SITE ?? "http://localhost:4321";
+const base = process.env.BASE || "/";
 
 // https://astro.build/config
 export default defineConfig({
   site,
+  base,
   integrations: [react(), sitemap(), icon()],
   experimental: {
     fonts: [

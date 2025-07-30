cae168442fe46a424ab3eec2d4254f54ac186c33
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 10:18:02 +0700

    feat: Set environment variable for GitHub Pages

--- astro.config.ts
+++ astro.config.ts
@@ -6,11 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
-const site = process.env.CI
-  ? process.env.VERCEL_ENV !== "production" && process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "https://torn4dom4n.github.io"
-  : "http://localhost:4321";
+const site = process.env.SITE;
+const base = process.env.BASE;
 
 // https://astro.build/config
 export default defineConfig({

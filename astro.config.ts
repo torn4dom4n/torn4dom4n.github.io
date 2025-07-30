53bb646d17e7bffb2f86e6fc993883d4e683a164
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-07-30 14:54:05 +0700

    better URL

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,15 @@
 // @ts-check
-
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
-const site = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : (process.env.SITE ?? "http://localhost:4321");
+const site =
+  process.env.SITE ??
+  (process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:4321");
 const base = "/";
 
 // https://astro.build/config

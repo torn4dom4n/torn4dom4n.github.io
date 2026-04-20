62de628b549287401fe2a2f02983f19ff86b3447
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-04-20 23:25:51 +0700

    Change font weight from bold to semibold

--- src/components/ui/section-header.tsx
+++ src/components/ui/section-header.tsx
@@ -11,7 +11,7 @@ export default function SectionHeader({
     <p
       className={cn(
         className,
-        "top-0 -left-10 origin-bottom-right text-left font-mono text-sm font-bold tracking-widest max-2xl:mb-4 max-2xl:px-2 max-sm:px-4 sm:text-xs 2xl:absolute 2xl:-translate-x-full 2xl:-translate-y-full 2xl:-rotate-90 2xl:text-right",
+        "top-0 -left-10 origin-bottom-right text-left font-mono text-sm font-semibold tracking-widest lowercase max-2xl:mb-4 max-2xl:px-2 max-sm:px-4 sm:text-xs 2xl:absolute 2xl:-translate-x-full 2xl:-translate-y-full 2xl:-rotate-90 2xl:text-right",
       )}
     >
       {children}

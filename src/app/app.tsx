099bcf59203e21b1816825b6e987be89be122acb
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-04-21 13:56:32 +0700

    Remove z-index from a div in app.tsx (#51)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -16,7 +16,7 @@ function App() {
       <main id="main-content" className="isolate">
         <div className="max-w-screen overflow-x-hidden">
           <div className="grid min-h-dvh grid-cols-1 grid-rows-[1fr_auto] justify-center [--gutter-width:2.5rem] md:-mx-4 md:grid-cols-[var(--gutter-width)_minmax(0,var(--breakpoint-2xl))_var(--gutter-width)] lg:mx-0">
-            <div className="relative z-10 col-start-1 row-span-full row-start-1 hidden border-x border-border md:block" />
+            <div className="relative col-start-1 row-span-full row-start-1 hidden border-x border-border md:block" />
 
             <div className="grid gap-24 pb-24 sm:gap-40 md:pb-40">
               <Hero />
